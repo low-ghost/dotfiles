5799e5cf12229cf78423c137c5004124d4bbbffa
Mike Bagwell <lowghost.clone@gmail.com>
2016-03-27 19:22:20 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ install='false'
 while getopts ':i' opt; do
 	case $opt in
 		i)
+			#install necessities
+			sudo apt-get install -y curl git
+			mkdir -p ~/repo
+			cd repo
 			#install neovim
 			sudo apt-get install -y software-properties-common
 			sudo add-apt-repository ppa:neovim-ppa/unstable
@@ -17,6 +21,20 @@ while getopts ':i' opt; do
 			sudo update-alternatives --config vim
 			sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 			sudo update-alternatives --config editor
+			
+			#install termite
+			git clone --recursive https://github.com/thestinger/termite.git
+			git clone https://github.com/thestinger/vte-ng.git
+			sudo apt-get install -y g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf
+			cd vte-ng && ./autogen.sh && make && sudo make install
+			cd ../termite && make && sudo make install
+			sudo desktop-file-install termite.desktop
+			sudo cp /usr/local/lib/libvte-2.91.a /usr/local/lib/libvte-2.91.la \
+			/usr/local/lib/libvte-2.91.so /usr/local/lib/libvte-2.91.so.0 \
+			/usr/local/lib/libvte-2.91.so.0.4200.4 /usr/lib
+			sudo mkdir -p /lib/terminfo/x; sudo ln -s \
+			/usr/local/share/terminfo/x/xterm-termite \
+			/lib/terminfo/x/xterm-termite
 			;;
 	esac
 done
