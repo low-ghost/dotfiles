5d895d38f0bc2f6fbe5be701bc6b33df9b5e19d4
Mike Bagwell <mbagwell@excelmicro.com>
2016-12-07 16:57:55 -0500

    little bit less hacky bash interop, but still pretty bad

--- bin/pgx.py
+++ bin/pgx.py
@@ -14,25 +14,28 @@ import argparse
 from os import environ
 from subprocess import run, PIPE, Popen
 from re import escape, compile
+from textwrap import dedent
 
 
 def get_args():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--file', help='file to execute',
+    parser.add_argument('-f', '--file', help='file to execute',
                         type=argparse.FileType('r'))
-    parser.add_argument('--environment', nargs='?',
+    parser.add_argument('-e', '--environment', nargs='?',
                         choices=['p', 's', 'd'], default='s',
                         help='environment for execution')
-    parser.add_argument('--user', nargs='?',
+    parser.add_argument('-u', '--user', nargs='?',
                         help='override environment based user')
-    parser.add_argument('--db', nargs='?',
+    parser.add_argument('-d', '--db', nargs='?',
                         help='override environment based db')
-    parser.add_argument('--location', nargs='?',
+    parser.add_argument('-l', '--location', nargs='?',
                         help='override environment based location (host)')
-    parser.add_argument('--getcommand', action='store_true',
-                        help='get command to execute for inspection or later execution')
-    parser.add_argument('--variables', nargs='+',
-                        help='list of variables to replace ${1} style prepared arguments')
+    parser.add_argument('-b', '--bash', action='store_true',
+                        help='used for hacky bash interop only (used by pgx script)')
+    parser.add_argument('-v', '--variables', nargs='+',
+                        help='list of variables to replace $1 style prepared arguments')
+    parser.add_argument('-p', '--print', action='store_true',
+                        help='print the sql itself')
     parser.add_argument('sql', help='sql to execute', nargs='?')
     parser.add_argument('jq', help='jq filter', default='.', nargs='?')
     return parser.parse_args()
@@ -49,21 +52,25 @@ def get_env(environment):
             'd': 'exm-production'}
 
 
-def get_replacement_dict(variables):
-    return { escape('${}'.format(k)): v for k, v in enumerate(variables, 1) }
-
-
 def make_replacements(variables, text):
-    if not variables:
-      return text
-
-    replacement_dict = get_replacement_dict(variables)
-    pattern = compile("|".join(replacement_dict.keys()))
-    return pattern.sub(lambda match: replacement_dict[escape(match.group(0))], text)
+    for i, v in enumerate(variables, 1):
+         string_key = '${}'.format(i)
+         text = text.replace(string_key, str(v))
+    return text
 
 
 def main():
     args = get_args()
+
+    sql = (args.file and args.file.read()) or args.sql or None
+
+    enter_pg_directly = sql is None
+
+    # Probably, the invoking script will re-invoke the script without --bash but
+    # otherwise the same args. We're exiting early to say, 'you can use pgx.py
+    # as is and not worry about stdin/out oddities'
+    if args.bash and not enter_pg_directly:
+        return print('continue')
 
     # If values are provided in args, assign them to the retrieved env
     # dictionary, otherwise, keep the retrieved values
@@ -74,25 +81,29 @@ def main():
         **{k: v for k, v in to_assign.items() if v is not None }}
 
     psql_command = 'psql -h {h} -U {u} -d {d}'.format(**env_dict)
-    sql = (args.file and args.file.read()) or args.sql or None
-    # Bit of a hack, but if file is provided, then first positional arg is jq
-    jq = args.sql if args.file else args.jq
 
-    if sql is None:
-        if args.getcommand:
+    if enter_pg_directly:
+        # Bash will want the command to get into the db
+        if args.bash:
             return print(psql_command)
+        # Use at own risk. This is why all the bash jumping around
         return run([psql_command], shell=True)
 
     replaced_sql = make_replacements(args.variables, sql)
-    command = ('{} -t -c'
-               ' "SELECT array_to_json(array_agg(row_to_json(fooo)))'
-               ' FROM ({}) AS fooo"'.format(psql_command, replaced_sql))
+    command = ('{} -t << EOF\n'
+               'SELECT array_to_json(array_agg(row_to_json(fooo)))'
+               'FROM ({}) AS fooo'
+               '\nEOF'.format(psql_command, replaced_sql))
 
-    if args.getcommand:
-        return print('{} | jq {}'.format(command, jq))
+    if args.print:
+        return print(replaced_sql)
 
     result = run([command], stdout=PIPE, shell=True)
+
+    # Bit of a hack, but if file is provided, then first positional arg is jq
+    jq = args.sql or '.' if args.file else args.jq
     p = Popen(['jq', jq], stdin=PIPE)
     return p.communicate(input=result.stdout)
 
 main()
+exit(0)
